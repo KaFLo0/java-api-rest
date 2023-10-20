130e96c4e103ea858d48b260052c76729f1fb25f
kaflo <kaiodavy7@gmail.com>
2023-10-20 16:29:57 -0300

    implementação da camada de services

--- src/main/java/io/kaflo/service/UserService.java
+++ src/main/java/io/kaflo/service/UserService.java
@@ -1,4 +1,11 @@
 package io.kaflo.service;
 
-public class UserService {
+import io.kaflo.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+
 }
--- src/main/java/io/kaflo/service/impl/UserServiceImpl.java
+++ src/main/java/io/kaflo/service/impl/UserServiceImpl.java
@@ -1,4 +1,31 @@
 package io.kaflo.service.impl;
 
-public class UserServiceImpl {
+import io.kaflo.domain.model.User;
+import io.kaflo.domain.repository.UserRepository;
+import io.kaflo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
 }
